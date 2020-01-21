8f7e36da0ddca08554716968cdb48486b7aae366
MaxxieMax <Max.breunesse@gmail.com>
2020-01-21 13:35:07 +0100

    alles klaar

--- game.php
+++ game.php
@@ -1,6 +1,6 @@
 <div class="col-12">
 
-<iframe src="https://i.simmer.io/@MaxxieMax/testr" style="width:960px;height:600px"></iframe>
+<iframe src="https://i.simmer.io/@MaxxieMax/testr" style="width:1080px;height:600px"></iframe>
 
 </div>
 
